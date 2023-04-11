7abe6bc3e5ee90bdc60e057f793d32ac815f5969
Ransom Sunday <sundayyoung64@gmail.com>
2023-04-11 21:04:08 +0100

    more practice on the fundamentals of javaScript

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+let name = "Ransom";
+name = "Herrayo";
+console.log(name);
+
+var fullName = "Chioma ochornongo"
+fullName = "Peter Edwin"
+console.log(fullName);
+
+const lastName = "sunday";
+lastName = "opewoye";
+console.log(lastName);
--- main.js
+++
@@ -1,4 +0,0 @@
-
-var firstName = "Ransom";
-var lastLetterOfFirstName = firstName[firstName.length-4]
-console.log(lastLetterOfFirstName);
