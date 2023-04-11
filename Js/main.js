5dd16a22285cde9fdedc8bc6fb669f0ad851a0ed
Ransom Sunday <sundayyoung64@gmail.com>
2023-04-11 21:46:19 +0100

    string concatenatio

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,13 @@
-const name = 'Ransom\'s life is in God\'s hand';
-console.log(name);
 
+// string concatenation - combine string values
+// `` - backticks (tmeplate strings) easier option.
+
+// const name = "Herryayo"
+// const lastName = "opewoye"
+// let fullName =' ' + name + ' ' + lastName;
+
+// console.log('Hello  your name is :' + fullName);
+
+const website = "google";
+const url = 'https:www.' + website + '.com';
+console.log(url);;
