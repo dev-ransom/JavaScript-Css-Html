0a3f1619c4053695bc249ddbd6f3461ed6a24e09
Ransom Sunday <sundayyoung64@gmail.com>
2023-04-27 09:16:35 +0100

    added code on accessing array element

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,10 @@
 const friends = ['Herryayo','Chioma','Famous','Emmanuel','Jeff','Ransom',23,undefined,null];
 let bestFriend = friends[1];
+friends[6] = 'Feranmi';
+console.log(friends[6]);
+console.log(friends);
 console.log(bestFriend);
 console.log(friends[3]);
+
+const person = {firstName:"John", lastName:"Doe", age:46};
+console.log(person.age);
