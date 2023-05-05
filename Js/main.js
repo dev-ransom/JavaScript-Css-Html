efc5ae6a8cb2b72e39ff470617f681138a14570c
Ransom Sunday <sundayyoung64@gmail.com>
2023-05-05 11:18:49 +0100

    added pen files

--- Js/main.js
+++ Js/main.js
@@ -1,14 +1,33 @@
-function addValues(num1, num2){
-    return num1 + num2;
+
+
+function gradingStudents(grades) {
+    // Write your code here
+    for (let i = 0; i < grades.length; i++){
+        if ((grades[i] >= 38) && ((((grades[i] % 5) == 3) || ((grades[i] % 5) == 4)))){
+            grades[i] = grades[i] + (5 - (grades[i]) % 5);
+        }
+    }
+    return grades;
 }
 
-const firstValue = addValues(3,8);
-const secondValue = addValues(9,8);
 
-const add = function (num1, num2){
-    return num1 + num2;
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
 }
-const thirdValue = add(78, 9);
-const values = [firstValue, secondValue, thirdValue];
-console.log(values);
-
