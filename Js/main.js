8c8d1e106ce9a2b162657ce2212e917c0f1f979f
Ransom Sunday <sundayyoung64@gmail.com>
2023-05-10 09:48:38 +0100

    added file for login validation

--- Js/main.js
+++ Js/main.js
@@ -12,16 +12,34 @@ else {
     console.log(notElig);
 }
 
-// Qualification for driver license
+// // Qualification for driver license
 
-let age = 20;
-let hasLicense = true;
-let hasCar = false;
+// let age = 20;
+// let hasLicense = true;
+// let hasCar = false;
 
-if (age >= 18 && hasLicense && !hasCar){
-    console.log("You can get a driver's license");
+// if (age >= 18 && hasLicense && !hasCar){
+//     console.log("You can get a driver's license");
+// }
+// else{
+//     console.log("You can't get a driver's license");
+
+// }
+
+const user = {
+    email: "example@gmail.com",
+    password: "password123",
+    acceptedTerms: true
+};
+
+const isValidLogin = 
+user.email.includes("@") && 
+user.password.length >= 8 && 
+user.acceptedTerms;
+
+if (isValidLogin){
+    console.log("login successful");
 }
-else{
-    console.log("You can't get a driver's license");
-
+else {
+    console.log("invalid login");
 }
