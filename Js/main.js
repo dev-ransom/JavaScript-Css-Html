b75692b1fcacf8be122749c1ee4329942dffe328
Ransom Sunday <sundayyoung64@gmail.com>
2023-05-15 12:57:40 +0100

    added code for template literals ES6+ and interpolation

--- Js/main.js
+++ Js/main.js
@@ -1,59 +1,6 @@
-
-// eligibility for discount
-const isNewCustomer = true;
-const totalPurchaseAmount = 120;
-const isEligibleForDiscount = isNewCustomer || totalPurchaseAmount > 100;
-const Eligible = "you are eligible for a discount!";
-const notElig = "you don't meet the requirements";
-if (isEligibleForDiscount){
-    console.log(Eligible)
-}
-else {
-    console.log(notElig);
-}
-
-// // Qualification for driver license
-
-// let age = 20;
-// let hasLicense = true;
-// let hasCar = false;
-
-// if (age >= 18 && hasLicense && !hasCar){
-//     console.log("You can get a driver's license");
-// }
-// else{
-//     console.log("You can't get a driver's license");
-
-// }
-
-const user = {
-    email: "example@gmail.com",
-    password: "password123",
-    acceptedTerms: true
-};
-
-const isValidLogin = 
-user.email.includes("@") && 
-user.password.length >= 8 && 
-user.acceptedTerms;
-
-if (isValidLogin){
-    console.log("login successful");
-}
-else {
-    console.log("invalid login");
-}
-
-
-
-let text = "Ransom Sunday"
-const relenght = text.length;
-const result = text.slice(0, 6);
-// console.log(text.length);
-// console.log(text.toLowerCase());
-// console.log(text.toUpperCase());
-// console.log(text.charAt(result-1));
-// console.log(text.indexOf('o'));
-// console.log(text.trim());
-
-document.getElementById("practice").innerHTML = result;
+const name = "Ransom Sunday"
+const age = 21;
+const sentence = "Hey it's " + name + " he is " + age + " years old";
+const value = `Hey it's ${name} he is ${age} years old`
+document.getElementById("practice").innerHTML = value;
+console.log(sentence)
